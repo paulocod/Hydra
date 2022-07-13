df284a3406f2dc545cc38f3d5cfa9b936fa5bdfe
PauloCampos <paulo4-0@outlook.com>
2022-07-13 15:11:27 -0300

    connect the prisma and mongodb atlas and seed the cluster

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,33 @@
 import express from "express";
+import prisma from "./prisma";
 
 
 const app = express()
 
 app.use(express.json())
+
+async function createContact() {
+  await prisma.$connect()
+
+  await prisma.contacts.create({
+    data: {
+      email: 'hello@prisma.com',
+      tags: {
+        create: {
+          title: '1dom',
+        },
+      },
+    },
+  })
+
+  const createWithSuccess = await prisma.contacts.findFirst({
+    include: {
+      tags: true,
+    },
+  })
+  console.log(createWithSuccess, { depth: null })
+}
+
+createContact()
 
 export default app
